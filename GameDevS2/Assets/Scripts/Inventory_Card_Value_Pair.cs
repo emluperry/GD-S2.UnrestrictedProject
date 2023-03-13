36eb8c6f780a76b41f436fac84459283b5e4132e
Emily Perry <emluperry@gmail.com>
2023-03-13 15:50:03 +0000

    Started to create further card dev

---
+++ GameDevS2/Assets/Scripts/Inventory_Card_Value_Pair.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Inventory_Card_Value_Pair : MonoBehaviour
+{
+    public Scriptable_Card card;
+    public int amount;
+}
---
+++ GameDevS2/Assets/Scripts/Player/PlayerCards.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCards : MonoBehaviour
+{
+    [SerializeField] private Inventory_Card_Value_Pair[] _cards;
+}
